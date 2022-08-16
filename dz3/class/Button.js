93ed7a4204bce6f0965ed0330cbaec4179424caf
Ivan Mikheev <drowjajka@gmail.com>
2022-08-16 17:29:37 +0700

    пишу class button

--- dz3/class/Button.js
+++ dz3/class/Button.js
@@ -1,7 +1,20 @@
 class Button {
-
+    constructor(width, height, background, value) {
+        this.width = width;
+        this.height = height;
+        this.background = background;
+        this.value = value;
+    }
 
     render() {
-
+        let btn = document.createElement("button");
+        btn.style.width = `${this.width}px`;
+        btn.style.height = `${this.height}px`;
+        btn.style.background = this.background;
+        btn.style.value = this.value;
+        return btn
     }
 }
+
+let button = new Button(200, 60, '#dff0d8', 'Ты создал кнопку');
+document.body.append(button.render());
